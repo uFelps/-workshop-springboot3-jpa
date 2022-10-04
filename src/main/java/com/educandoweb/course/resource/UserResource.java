a09b1ef5a2957b496e102a695d78a3e1bb3d09e2
Fulano de Tal <felipesand824@gmail.com>
2022-10-04 20:22:27 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/course/resource/UserResource.java
@@ -0,0 +1,21 @@
+package com.educandoweb.course.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "felipe", "felipe@gmail.com", "9944499772", "1234");
+		
+		return ResponseEntity.ok().body(u);
+	}
+
+}
